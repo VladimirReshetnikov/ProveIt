c6b1a02c0d3a7e9cc5510c7298cdec5d18494677
Vladimir Reshetnikov <v.reshetnikov@gmail.com>
2026-08-01 08:13:48 -0700

    Complete open-induction projection definability
    Port Foundation pi1Def and pi2Def as their source bounded-existential Sigma-zero graphs, reusing the checked pairing graph through explicit variable reindexings.
    
    Factor the finite-environment permutations into reusable lemmas, prove both graph formulas characterize the semantic projections by pairing bijectivity, and promote the definitions uniformly through every arithmetic hierarchy symbol.
    
    Package the sharp projection-at-most-input bounds as term majorants, expose combined definably-bounded witnesses, register the declarations in the assumption audit, and promote IOpen/Basic.lean from partial to fully ported.

--- Logic/FoundationPort/Coq/FirstOrder/Arithmetic/IOpen/Definability.v
+++ Logic/FoundationPort/Coq/FirstOrder/Arithmetic/IOpen/Definability.v
@@ -12,7 +12,7 @@ From Stdlib Require Import Vectors.Fin.
 From Foundation.Syntax.Predicate Require Import Language Term Rew.
 From Foundation.FirstOrder.Basic.Syntax Require Import Formula.
 From Foundation.FirstOrder.Basic Require Import Operator.
-From Foundation.FirstOrder.Basic.Semantics Require Import Semantics.
+From Foundation.FirstOrder.Basic.Semantics Require Import Semantics Elementary.
 From Foundation.FirstOrder.Arithmetic.Basic Require Import Misc Syntax Hierarchy.
 From Foundation.FirstOrder.Arithmetic Require Import Schemata.
 From Foundation.FirstOrder.Arithmetic.Definability Require Import
@@ -593,4 +593,360 @@ Proof.
   intros M Str O Horing Hpa. constructor.
   - exact (arithmetic_iopen_pair_bounded Horing Hpa).
   - exact (arithmetic_iopen_pair_definable_zero Horing Hpa).
+Defined.
+
+(** * Pairing projections *)
+
+Definition arithmetic_iopen_pi1_pair_instance : arithmetic_semisentence 3 :=
+  semiformula_rewrite
+    (rew_map
+      (peano_minus_fin_three
+        (Fin.FS (Fin.FS Fin.F1)) (Fin.FS Fin.F1) Fin.F1)
+      (fun x : Empty_set => match x with end))
+    arithmetic_iopen_pair_graph_formula.
+
+Definition arithmetic_iopen_pi2_pair_instance : arithmetic_semisentence 3 :=
+  semiformula_rewrite
+    (rew_map
+      (peano_minus_fin_three
+        (Fin.FS (Fin.FS Fin.F1)) Fin.F1 (Fin.FS Fin.F1))
+      (fun x : Empty_set => match x with end))
+    arithmetic_iopen_pair_graph_formula.
+
+Definition arithmetic_iopen_pi1_graph_formula : arithmetic_semisentence 2 :=
+  peano_minus_bex_lt_succ
+    (@Semiterm_bvar oring_language Empty_set 2 (Fin.FS Fin.F1))
+    arithmetic_iopen_pi1_pair_instance.
+
+Definition arithmetic_iopen_pi2_graph_formula : arithmetic_semisentence 2 :=
+  peano_minus_bex_lt_succ
+    (@Semiterm_bvar oring_language Empty_set 2 (Fin.FS Fin.F1))
+    arithmetic_iopen_pi2_pair_instance.
+
+Lemma arithmetic_iopen_pi1_graph_formula_hierarchy :
+  arithmetic_hierarchy Empty_set arithmetic_sigma 0 2
+    arithmetic_iopen_pi1_graph_formula.
+Proof.
+  unfold arithmetic_iopen_pi1_graph_formula, peano_minus_bex_lt_succ.
+  apply (proj2 (@arithmetic_hierarchy_bex_lt_succ_iff Empty_set
+    arithmetic_sigma 0 2
+    (@Semiterm_bvar oring_language Empty_set 2 (Fin.FS Fin.F1))
+    arithmetic_iopen_pi1_pair_instance)).
+  unfold arithmetic_iopen_pi1_pair_instance.
+  exact (@arithmetic_hierarchy_rewrite Empty_set arithmetic_sigma 0 3
+    arithmetic_iopen_pair_graph_formula
+    arithmetic_iopen_pair_graph_formula_hierarchy Empty_set 3
+    (rew_map
+      (peano_minus_fin_three
+        (Fin.FS (Fin.FS Fin.F1)) (Fin.FS Fin.F1) Fin.F1)
+      (fun x : Empty_set => match x with end))).
+Qed.
+
+Lemma arithmetic_iopen_pi2_graph_formula_hierarchy :
+  arithmetic_hierarchy Empty_set arithmetic_sigma 0 2
+    arithmetic_iopen_pi2_graph_formula.
+Proof.
+  unfold arithmetic_iopen_pi2_graph_formula, peano_minus_bex_lt_succ.
+  apply (proj2 (@arithmetic_hierarchy_bex_lt_succ_iff Empty_set
+    arithmetic_sigma 0 2
+    (@Semiterm_bvar oring_language Empty_set 2 (Fin.FS Fin.F1))
+    arithmetic_iopen_pi2_pair_instance)).
+  unfold arithmetic_iopen_pi2_pair_instance.
+  exact (@arithmetic_hierarchy_rewrite Empty_set arithmetic_sigma 0 3
+    arithmetic_iopen_pair_graph_formula
+    arithmetic_iopen_pair_graph_formula_hierarchy Empty_set 3
+    (rew_map
+      (peano_minus_fin_three
+        (Fin.FS (Fin.FS Fin.F1)) Fin.F1 (Fin.FS Fin.F1))
+      (fun x : Empty_set => match x with end))).
+Qed.
+
+Definition arithmetic_iopen_pi1_graph_sorted :
+    arithmetic_sorted_formula Empty_set 2 arithmetic_sigma_zero_symbol :=
+  ArithmeticSortedSigma 0 arithmetic_iopen_pi1_graph_formula
+    arithmetic_iopen_pi1_graph_formula_hierarchy.
+
+Definition arithmetic_iopen_pi2_graph_sorted :
+    arithmetic_sorted_formula Empty_set 2 arithmetic_sigma_zero_symbol :=
+  ArithmeticSortedSigma 0 arithmetic_iopen_pi2_graph_formula
+    arithmetic_iopen_pi2_graph_formula_hierarchy.
+
+Lemma arithmetic_iopen_pi1_pair_env : forall M (v : Fin.t 2 -> M) y i,
+  fin_env_cons y v
+      (peano_minus_fin_three
+        (Fin.FS (Fin.FS Fin.F1)) (Fin.FS Fin.F1) Fin.F1 i) =
+  peano_minus_fin_three (v (Fin.FS Fin.F1)) (v Fin.F1) y i.
+Proof.
+  intros M v y i.
+  refine (@Fin.caseS' 2 i (fun j =>
+    fin_env_cons y v
+        (peano_minus_fin_three
+          (Fin.FS (Fin.FS Fin.F1)) (Fin.FS Fin.F1) Fin.F1 j) =
+    peano_minus_fin_three (v (Fin.FS Fin.F1)) (v Fin.F1) y j) _ _).
+  - reflexivity.
+  - intro j. refine (@Fin.caseS' 1 j (fun q =>
+      fin_env_cons y v
+          (peano_minus_fin_three
+            (Fin.FS (Fin.FS Fin.F1)) (Fin.FS Fin.F1) Fin.F1 (Fin.FS q)) =
+      peano_minus_fin_three
+        (v (Fin.FS Fin.F1)) (v Fin.F1) y (Fin.FS q)) _ _).
+    + reflexivity.
+    + intro q. refine (@Fin.caseS' 0 q (fun r =>
+        fin_env_cons y v
+            (peano_minus_fin_three
+              (Fin.FS (Fin.FS Fin.F1)) (Fin.FS Fin.F1) Fin.F1
+              (Fin.FS (Fin.FS r))) =
+        peano_minus_fin_three
+          (v (Fin.FS Fin.F1)) (v Fin.F1) y
+          (Fin.FS (Fin.FS r))) _ _).
+      * reflexivity.
+      * intro r. inversion r.
+Qed.
+
+Lemma arithmetic_iopen_pi2_pair_env : forall M (v : Fin.t 2 -> M) x i,
+  fin_env_cons x v
+      (peano_minus_fin_three
+        (Fin.FS (Fin.FS Fin.F1)) Fin.F1 (Fin.FS Fin.F1) i) =
+  peano_minus_fin_three (v (Fin.FS Fin.F1)) x (v Fin.F1) i.
+Proof.
+  intros M v x i.
+  refine (@Fin.caseS' 2 i (fun j =>
+    fin_env_cons x v
+        (peano_minus_fin_three
+          (Fin.FS (Fin.FS Fin.F1)) Fin.F1 (Fin.FS Fin.F1) j) =
+    peano_minus_fin_three (v (Fin.FS Fin.F1)) x (v Fin.F1) j) _ _).
+  - reflexivity.
+  - intro j. refine (@Fin.caseS' 1 j (fun q =>
+      fin_env_cons x v
+          (peano_minus_fin_three
+            (Fin.FS (Fin.FS Fin.F1)) Fin.F1 (Fin.FS Fin.F1) (Fin.FS q)) =
+      peano_minus_fin_three
+        (v (Fin.FS Fin.F1)) x (v Fin.F1) (Fin.FS q)) _ _).
+    + reflexivity.
+    + intro q. refine (@Fin.caseS' 0 q (fun r =>
+        fin_env_cons x v
+            (peano_minus_fin_three
+              (Fin.FS (Fin.FS Fin.F1)) Fin.F1 (Fin.FS Fin.F1)
+              (Fin.FS (Fin.FS r))) =
+        peano_minus_fin_three
+          (v (Fin.FS Fin.F1)) x (v Fin.F1)
+          (Fin.FS (Fin.FS r))) _ _).
+      * reflexivity.
+      * intro r. inversion r.
+Qed.
+
+Lemma arithmetic_iopen_pi1_pair_instance_eval : forall M
+    (Str : first_order_structure oring_language M) (O : oring_carrier M),
+  structure_interprets_oring Str oring_language_structure O ->
+  peano_minus_laws O -> forall (v : Fin.t 2 -> M) y,
+  semiformula_eval Str (fin_env_cons y v)
+    (fun x : Empty_set => match x with end)
+    arithmetic_iopen_pi1_pair_instance <->
+  v (Fin.FS Fin.F1) = iopen_pair O (v Fin.F1) y.
+Proof.
+  intros M Str O Horing Hpa v y.
+  unfold arithmetic_iopen_pi1_pair_instance.
+  rewrite semiformula_eval_map.
+  etransitivity.
+  - apply semiformula_eval_bound_extensional.
+    apply arithmetic_iopen_pi1_pair_env.
+  - etransitivity.
+    + apply semiformula_eval_free_ext. intros x _. destruct x.
+    + exact (arithmetic_iopen_pair_graph_formula_eval Horing Hpa
+        (peano_minus_fin_three (v (Fin.FS Fin.F1)) (v Fin.F1) y)).
+Qed.
+
+Lemma arithmetic_iopen_pi2_pair_instance_eval : forall M
+    (Str : first_order_structure oring_language M) (O : oring_carrier M),
+  structure_interprets_oring Str oring_language_structure O ->
+  peano_minus_laws O -> forall (v : Fin.t 2 -> M) x,
+  semiformula_eval Str (fin_env_cons x v)
+    (fun z : Empty_set => match z with end)
+    arithmetic_iopen_pi2_pair_instance <->
+  v (Fin.FS Fin.F1) = iopen_pair O x (v Fin.F1).
+Proof.
+  intros M Str O Horing Hpa v x.
+  unfold arithmetic_iopen_pi2_pair_instance.
+  rewrite semiformula_eval_map.
+  etransitivity.
+  - apply semiformula_eval_bound_extensional.
+    apply arithmetic_iopen_pi2_pair_env.
+  - etransitivity.
+    + apply semiformula_eval_free_ext. intros z _. destruct z.
+    + exact (arithmetic_iopen_pair_graph_formula_eval Horing Hpa
+        (peano_minus_fin_three (v (Fin.FS Fin.F1)) x (v Fin.F1))).
+Qed.
+
+Theorem arithmetic_iopen_pi1_graph_formula_eval : forall M
+    (Str : first_order_structure oring_language M) (O : oring_carrier M),
+  structure_interprets_oring Str oring_language_structure O ->
+  peano_minus_laws O -> arithmetic_least_number_principle O ->
+  forall v : Fin.t 2 -> M,
+  semiformula_eval Str v (fun x : Empty_set => match x with end)
+    arithmetic_iopen_pi1_graph_formula <->
+  v Fin.F1 = iopen_pi1 O (v (Fin.FS Fin.F1)).
+Proof.
+  intros M Str O Horing Hpa Hleast v.
+  unfold arithmetic_iopen_pi1_graph_formula.
+  rewrite (peano_minus_eval_bex_lt_succ Horing Hpa).
+  cbn [semiterm_val].
+  setoid_rewrite (arithmetic_iopen_pi1_pair_instance_eval Horing Hpa).
+  split.
+  - intros [y [_ Hpair]].
+    rewrite Hpair, (iopen_pi1_pair Hpa Hleast). reflexivity.
+  - intro Hpi. exists (iopen_pi2 O (v (Fin.FS Fin.F1))). split.
+    + apply (iopen_pi2_le_self Hpa Hleast).
+    + rewrite Hpi. symmetry. apply (iopen_pair_pi Hpa Hleast).
+Qed.
+
+Theorem arithmetic_iopen_pi2_graph_formula_eval : forall M
+    (Str : first_order_structure oring_language M) (O : oring_carrier M),
+  structure_interprets_oring Str oring_language_structure O ->
+  peano_minus_laws O -> arithmetic_least_number_principle O ->
+  forall v : Fin.t 2 -> M,
+  semiformula_eval Str v (fun x : Empty_set => match x with end)
+    arithmetic_iopen_pi2_graph_formula <->
+  v Fin.F1 = iopen_pi2 O (v (Fin.FS Fin.F1)).
+Proof.
+  intros M Str O Horing Hpa Hleast v.
+  unfold arithmetic_iopen_pi2_graph_formula.
+  rewrite (peano_minus_eval_bex_lt_succ Horing Hpa).
+  cbn [semiterm_val].
+  setoid_rewrite (arithmetic_iopen_pi2_pair_instance_eval Horing Hpa).
+  split.
+  - intros [x [_ Hpair]].
+    rewrite Hpair, (iopen_pi2_pair Hpa Hleast). reflexivity.
+  - intro Hpi. exists (iopen_pi1 O (v (Fin.FS Fin.F1))). split.
+    + apply (iopen_pi1_le_self Hpa Hleast).
+    + rewrite Hpi. symmetry. apply (iopen_pair_pi Hpa Hleast).
+Qed.
+
+Definition arithmetic_iopen_pi1_defined : forall M
+    (Str : first_order_structure oring_language M) (O : oring_carrier M),
+  structure_interprets_oring Str oring_language_structure O ->
+  peano_minus_laws O -> arithmetic_least_number_principle O ->
+  arithmetic_sorted_defined_function Str
+    (fun v : Fin.t 1 -> M => iopen_pi1 O (v Fin.F1))
+    arithmetic_iopen_pi1_graph_sorted.
+Proof.
+  intros M Str O Horing Hpa Hleast.
+  unfold arithmetic_sorted_defined_function. constructor. split.
+  - exact I.
+  - intro v. apply (arithmetic_iopen_pi1_graph_formula_eval
+      Horing Hpa Hleast).
+Defined.
+
+Definition arithmetic_iopen_pi2_defined : forall M
+    (Str : first_order_structure oring_language M) (O : oring_carrier M),
+  structure_interprets_oring Str oring_language_structure O ->
+  peano_minus_laws O -> arithmetic_least_number_principle O ->
+  arithmetic_sorted_defined_function Str
+    (fun v : Fin.t 1 -> M => iopen_pi2 O (v Fin.F1))
+    arithmetic_iopen_pi2_graph_sorted.
+Proof.
+  intros M Str O Horing Hpa Hleast.
+  unfold arithmetic_sorted_defined_function. constructor. split.
+  - exact I.
+  - intro v. apply (arithmetic_iopen_pi2_graph_formula_eval
+      Horing Hpa Hleast).
+Defined.
+
+Definition arithmetic_iopen_pi1_definable_zero : forall M
+    (Str : first_order_structure oring_language M) (O : oring_carrier M),
+  structure_interprets_oring Str oring_language_structure O ->
+  peano_minus_laws O -> arithmetic_least_number_principle O ->
+  arithmetic_sorted_definable_function Str arithmetic_sigma_zero_symbol
+    (fun v : Fin.t 1 -> M => iopen_pi1 O (v Fin.F1)).
+Proof.
+  intros M Str O Horing Hpa Hleast.
+  unfold arithmetic_sorted_definable_function.
+  exact (arithmetic_sorted_defined_to_definable
+    (arithmetic_iopen_pi1_defined Horing Hpa Hleast)).
+Defined.
+
+Definition arithmetic_iopen_pi2_definable_zero : forall M
+    (Str : first_order_structure oring_language M) (O : oring_carrier M),
+  structure_interprets_oring Str oring_language_structure O ->
+  peano_minus_laws O -> arithmetic_least_number_principle O ->
+  arithmetic_sorted_definable_function Str arithmetic_sigma_zero_symbol
+    (fun v : Fin.t 1 -> M => iopen_pi2 O (v Fin.F1)).
+Proof.
+  intros M Str O Horing Hpa Hleast.
+  unfold arithmetic_sorted_definable_function.
+  exact (arithmetic_sorted_defined_to_definable
+    (arithmetic_iopen_pi2_defined Horing Hpa Hleast)).
+Defined.
+
+Definition arithmetic_iopen_pi1_definable : forall M
+    (Str : first_order_structure oring_language M) (O : oring_carrier M),
+  structure_interprets_oring Str oring_language_structure O ->
+  peano_minus_laws O -> arithmetic_least_number_principle O -> forall symbol,
+  arithmetic_sorted_definable_function Str symbol
+    (fun v : Fin.t 1 -> M => iopen_pi1 O (v Fin.F1)).
+Proof.
+  intros M Str O Horing Hpa Hleast symbol.
+  unfold arithmetic_sorted_definable_function.
+  exact (arithmetic_sorted_definable_of_zero
+    (arithmetic_iopen_pi1_definable_zero Horing Hpa Hleast) symbol).
+Defined.
+
+Definition arithmetic_iopen_pi2_definable : forall M
+    (Str : first_order_structure oring_language M) (O : oring_carrier M),
+  structure_interprets_oring Str oring_language_structure O ->
+  peano_minus_laws O -> arithmetic_least_number_principle O -> forall symbol,
+  arithmetic_sorted_definable_function Str symbol
+    (fun v : Fin.t 1 -> M => iopen_pi2 O (v Fin.F1)).
+Proof.
+  intros M Str O Horing Hpa Hleast symbol.
+  unfold arithmetic_sorted_definable_function.
+  exact (arithmetic_sorted_definable_of_zero
+    (arithmetic_iopen_pi2_definable_zero Horing Hpa Hleast) symbol).
+Defined.
+
+Definition arithmetic_iopen_pi1_bounded : forall M
+    (Str : first_order_structure oring_language M) (O : oring_carrier M),
+  peano_minus_laws O -> arithmetic_least_number_principle O ->
+  arithmetic_bounded_function Str (peano_minus_le O)
+    (fun v : Fin.t 1 -> M => iopen_pi1 O (v Fin.F1)).
+Proof.
+  intros M Str O Hpa Hleast.
+  refine {| arithmetic_bounded_term :=
+    @Semiterm_bvar oring_language M 1 Fin.F1 |}.
+  intro v. cbn [semiterm_val]. apply (iopen_pi1_le_self Hpa Hleast).
+Defined.
+
+Definition arithmetic_iopen_pi2_bounded : forall M
+    (Str : first_order_structure oring_language M) (O : oring_carrier M),
+  peano_minus_laws O -> arithmetic_least_number_principle O ->
+  arithmetic_bounded_function Str (peano_minus_le O)
+    (fun v : Fin.t 1 -> M => iopen_pi2 O (v Fin.F1)).
+Proof.
+  intros M Str O Hpa Hleast.
+  refine {| arithmetic_bounded_term :=
+    @Semiterm_bvar oring_language M 1 Fin.F1 |}.
+  intro v. cbn [semiterm_val]. apply (iopen_pi2_le_self Hpa Hleast).
+Defined.
+
+Definition arithmetic_iopen_pi1_definably_bounded : forall M
+    (Str : first_order_structure oring_language M) (O : oring_carrier M),
+  structure_interprets_oring Str oring_language_structure O ->
+  peano_minus_laws O -> arithmetic_least_number_principle O ->
+  arithmetic_definably_bounded_function Str (peano_minus_le O)
+    (fun v : Fin.t 1 -> M => iopen_pi1 O (v Fin.F1)).
+Proof.
+  intros M Str O Horing Hpa Hleast. constructor.
+  - exact (arithmetic_iopen_pi1_bounded Str Hpa Hleast).
+  - exact (arithmetic_iopen_pi1_definable_zero Horing Hpa Hleast).
+Defined.
+
+Definition arithmetic_iopen_pi2_definably_bounded : forall M
+    (Str : first_order_structure oring_language M) (O : oring_carrier M),
+  structure_interprets_oring Str oring_language_structure O ->
+  peano_minus_laws O -> arithmetic_least_number_principle O ->
+  arithmetic_definably_bounded_function Str (peano_minus_le O)
+    (fun v : Fin.t 1 -> M => iopen_pi2 O (v Fin.F1)).
+Proof.
+  intros M Str O Horing Hpa Hleast. constructor.
+  - exact (arithmetic_iopen_pi2_bounded Str Hpa Hleast).
+  - exact (arithmetic_iopen_pi2_definable_zero Horing Hpa Hleast).
 Defined.
