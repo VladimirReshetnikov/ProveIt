9e207560e90dac36570655551dabdaf05f0e1b18
Vladimir Reshetnikov <v.reshetnikov@gmail.com>
2026-08-06 04:20:56 -0700

    Generalize mixed-radix term enumeration

--- Algebra/PolynomialFormulas/Coq/SexticMuRecMixedRadixSemantics.v
+++ Algebra/PolynomialFormulas/Coq/SexticMuRecMixedRadixSemantics.v
@@ -77,6 +77,19 @@ rewrite expnS divnMA divnMDl //.
 by rewrite (@divn_small digit b (valP digit)) addn0.
 Qed.
 
+Lemma radix_join_mod_digit (digit : 'I_b) (rest : nat) :
+  (((rest * b + digit)%N %% b)%N) = digit.
+Proof.
+by rewrite modnMDl modn_small //; exact: valP digit.
+Qed.
+
+Lemma radix_join_div_rest (hb : (0 < b)%N) (digit : 'I_b)
+    (rest : nat) :
+  (((rest * b + digit)%N %/ b)%N) = rest.
+Proof.
+by rewrite divnMDl // divn_small ?addn0 //; exact: valP digit.
+Qed.
+
 End RadixBijection.
 
 Section MixedRadix.
@@ -114,5 +127,78 @@ elim: n term => [|n ih] term hb.
 Qed.
 
 End MixedRadix.
+
+Section ObserverBridge.
+
+Variable T : Type.
+
+Fixpoint mixed_radix_term_from (one : T) (mul : T -> T -> T)
+    (digit_term : nat -> nat -> T) (b position count code : nat) : T :=
+  match count with
+  | 0 => one
+  | count'.+1 =>
+      mul (digit_term position ((code %% b)%N))
+        (mixed_radix_term_from one mul digit_term b position.+1 count'
+          ((code %/ b)%N))
+  end.
+
+Fixpoint cartesian_terms (one : T) (mul : T -> T -> T)
+    (factors : seq (seq T)) : seq T :=
+  match factors with
+  | [::] => [:: one]
+  | head :: tail =>
+      flatten
+        [seq [seq mul x y | y <- cartesian_terms one mul tail]
+          | x <- head]
+  end.
+
+Variable R : comSemiRingType.
+
+Lemma mixed_radix_cartesian_observer_sum
+    (one : T) (mul : T -> T -> T) (digit_term : nat -> nat -> T)
+    (factor_terms : nat -> seq T) (observe : T -> R)
+    (b position count : nat) :
+  (0 < b)%N ->
+  (forall (i : nat) (target : T -> R),
+      \sum_(digit < b) target (digit_term i digit) =
+      \sum_(term <- factor_terms i) target term) ->
+  \sum_(code < b ^ count)
+      observe
+        (mixed_radix_term_from one mul digit_term b position count code) =
+  \sum_(term <- cartesian_terms one mul
+      [seq factor_terms i | i <- iota position count]) observe term.
+Proof.
+move=> hb hfactor.
+elim: count position observe => [|count ih] position observe.
+- by rewrite /= expn0 big_ord1 big_seq1.
+- rewrite /= expnS.
+  rewrite (reindex (@radix_join b (b ^ count))) /=;
+    last first.
+      apply: onW_bij.
+      exact: (@radix_join_bijective b (b ^ count) hb).
+  rewrite -(@pair_bigA R 0 +%R ('I_b) ('I_(b ^ count))
+    (fun digit rest =>
+      observe
+        (mixed_radix_term_from one mul digit_term b position count.+1
+          (rest * b + digit)%N))) /=.
+  under eq_bigr => digit _ do
+    under eq_bigr => rest _ do
+      rewrite /= (radix_join_mod_digit digit rest)
+        (radix_join_div_rest hb digit rest).
+  under eq_bigr => digit _ do
+    rewrite (ih position.+1
+      (fun tail_term =>
+        observe (mul (digit_term position digit) tail_term))).
+  rewrite (hfactor position
+    (fun x =>
+      \sum_(tail_term <- cartesian_terms one mul
+          [seq factor_terms i | i <- iota position.+1 count])
+        observe (mul x tail_term))).
+  rewrite big_flatten big_map.
+  apply: eq_bigr => term _.
+  by rewrite big_map.
+Qed.
+
+End ObserverBridge.
 
 End PolynomialFormulasSexticMuRecMixedRadixSemantics.
