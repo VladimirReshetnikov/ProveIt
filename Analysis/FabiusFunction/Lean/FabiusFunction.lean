9ce13666b4050ab4320c9d25bd882bfd756877b0
Vladimir Reshetnikov <v.reshetnikov@gmail.com>
2026-08-29 22:39:14 -0700

    formalize Legendre translate series

--- Analysis/FabiusFunction/Lean/FabiusFunction.lean
+++ Analysis/FabiusFunction/Lean/FabiusFunction.lean
@@ -318,6 +318,7 @@ import FabiusFunction.FabiusLegendreEnergy
 import FabiusFunction.FabiusLegendreRationalEnergy
 import FabiusFunction.FabiusSquareEnergyFourier
 import FabiusFunction.FabiusLegendreTranslateBlocks
+import FabiusFunction.FabiusLegendreTranslateSeries
 import FabiusFunction.FabiusBinaryReductionSeries
 import FabiusFunction.FabiusComputability
 import FabiusFunction.Monotonicity
