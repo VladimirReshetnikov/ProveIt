04bb59024943223de0ec37fba1dfb162c540bcc1
Vladimir Reshetnikov <v.reshetnikov@gmail.com>
2026-08-29 18:35:45 -0700

    wip: RademacherSine

--- Analysis/FabiusFunction/Lean/FabiusFunction.lean
+++ Analysis/FabiusFunction/Lean/FabiusFunction.lean
@@ -54,6 +54,7 @@ import FabiusFunction.ParityCharacter
 import FabiusFunction.ParityCharacterKernel
 import FabiusFunction.DigitCharacterCongruence
 import FabiusFunction.BinaryDigitFloor
+import FabiusFunction.RademacherSine
 import FabiusFunction.GeneralizedProuhetBlock
 import FabiusFunction.ProuhetBlockZeroOrder
 import FabiusFunction.BitPositionQBinomial
