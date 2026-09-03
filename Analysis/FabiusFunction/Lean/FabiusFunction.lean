1f6153ce376bafbff64e6024af7c438a5526eac2
Vladimir Reshetnikov <v.reshetnikov@gmail.com>
2026-09-02 20:23:51 -0700

    wip: AliasDyadicBridge draft

--- Analysis/FabiusFunction/Lean/FabiusFunction.lean
+++ Analysis/FabiusFunction/Lean/FabiusFunction.lean
@@ -248,6 +248,7 @@ import FabiusFunction.ThueMorseFourierInversion
 import FabiusFunction.HalfIntegerOddDFT
 import FabiusFunction.SummableCyclicAlias
 import FabiusFunction.HalfIntegerAliasIdentity
+import FabiusFunction.AliasDyadicBridge
 import FabiusFunction.CyclicCharacterSums
 import FabiusFunction.AliasErrorBound
 import FabiusFunction.DyadicDerivativeFiltration
