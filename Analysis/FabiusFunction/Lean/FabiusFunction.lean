2b6aeb5aa3a063b64f7fe482a1fd506276d64230
Vladimir Reshetnikov <v.reshetnikov@gmail.com>
2026-09-04 11:44:09 -0700

    wip: CayleyTreeFunction

--- Analysis/FabiusFunction/Lean/FabiusFunction.lean
+++ Analysis/FabiusFunction/Lean/FabiusFunction.lean
@@ -5,6 +5,7 @@ import FabiusFunction.PeriodicOrbitGelfondBound
 import FabiusFunction.PascalParityPeriodic
 import FabiusFunction.RvachevHalfIntegerEnergy
 import FabiusFunction.LambertWElementaryBounds
+import FabiusFunction.CayleyTreeFunction
 import FabiusFunction.CompleteHomogeneous
 import FabiusFunction.CompleteHomogeneousGenerating
 import FabiusFunction.CompleteHomogeneousBell
